e6a40cecd37bb2bffe1ae964ed5946d1fb444796
Darren Young <darren@yhlsecurity.com>
2021-03-15 16:27:12 -0500

    adjusted dest dir

--- code/templates/ubuntu18/scripts/cis-setup.sh
+++ code/templates/ubuntu18/scripts/cis-setup.sh
@@ -13,7 +13,7 @@ CIS_VER="cis_ubuntu_18.04"
 
 SRC="/tmp/$CIS_VER"
 DEST="/opt"
-CISDIR="/opt/$CIS_VER"
+CISDIR="/opt/cis"
 
 # Copy the CIS source to /opt
 echo "Copying CIS source files ($SRC to $DEST)"
